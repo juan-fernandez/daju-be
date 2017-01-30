68538210b65cebd38e593aa94d139244f988f7f7
Juan <fernandez.alba.juan@gmail.com>
2017-01-30 16:39:36 +0100

    new version with multiple users, position now controlled by the server at all times

--- index.js
+++ index.js
@@ -6,15 +6,61 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
+
+var players = [];
+
+
 function onConnection(socket){
 
-    socket.on('moving',(data)=>{
-        console.log("moving event",data)
-        socket.emit('moving',data) // to sender as well
-        socket.broadcast.emit('moving',data)
+
+    // missing checking if colliding with existing players
+    var new_player = {
+        id:socket.id,
+        pos:{
+            x:Math.floor((Math.random() * 450) + 1),
+            y:Math.floor((Math.random() * 450) + 1)
+        }
+    };
+
+    players.push(new_player)
+    console.log("New player created:",new_player);
+
+    // new player and all the players to redraw
+    socket.emit("new user",new_player,players) // to sender as well
+    socket.broadcast.emit("new user",new_player,players);
+
+
+    socket.on('moving',(movement)=>{
+        console.log("player moving:",movement)
+        // change user position
+        var index = players.findIndex((player)=>{
+            //console.log("player id:",player.id)
+            return player.id == movement.socket_id;
+        })
+        if(index!=-1){
+            players[index].pos.x += movement.vel_x;
+            players[index].pos.y += movement.vel_y;
+            console.log("sending players:",players)
+            socket.emit('moving',players) // to sender as well
+            socket.broadcast.emit('moving',players)
+        }
+    })
+    socket.on('disconnect',()=>{
+        console.log("player disco:",socket.id)
+        var index = players.findIndex((player)=>{
+            //console.log("player id:",player.id)
+            return player.id == socket.id;
+        })
+        if(index!= -1){
+            players.splice(index,1);
+        }
+        socket.broadcast.emit('delete user',players)
     })
 }
 
 io.on('connection', onConnection);
+
+
+
 
 http.listen(port, () => console.log('listening on port ' + port));
--- public/main.js
+++ public/main.js
@@ -2,56 +2,109 @@
 
 (function() {
 
-    var socket = io();
+
+
+/*    console.log("socket",socket)
     var canvas = document.getElementById('game_board');
     var context = canvas.getContext('2d');
     var drawing = false;
 
-    var INITIAL_POS_X = 0;
-    var INITIAL_POS_Y = 0;
-    var WIDTH = 100;
-    var LENGTH = 100;
+    var INITIAL_POS_X = Math.floor((Math.random() * 450) + 1);
+    var INITIAL_POS_Y = Math.floor((Math.random() * 450) + 1);
+    var WIDTH = 50;
+    var LENGTH = 50;
 
     var position_x = INITIAL_POS_X;
     var position_y = INITIAL_POS_Y;
 
 
-    /*initial rectangle*/
+    var colors = ['red','blue','green'];
+    var randomIndex = Math.floor((Math.random() * 3));
+
     context.beginPath();
     context.rect(position_x, position_y, WIDTH, LENGTH);
+    context.strokeStyle = colors[randomIndex];
     context.stroke();
     context.closePath();
 
+*/
 
-    socket.on('moving', function(data){
-        moveSquare(data);
+    var WIDTH = 50;
+    var LENGTH = 50;
+
+    var canvas = document.getElementById('game_board');
+    var context = canvas.getContext('2d');
+
+    var URL = "https://daju.herokuapp.com/";
+
+    //var URL = "http://localhost:3000/";
+
+    var socket = io(URL);
+    var my_id = null;
+
+    socket.on("connect",function(){
+        my_id = socket.id;
+    })
+
+
+    socket.on("new user",function(new_player, players){
+        // make same announcment
+        updateSquares(players);
+    })
+
+    socket.on("delete user",function(players){
+        // make same announcment
+        updateSquares(players);
+    })
+
+
+    socket.on('moving', function(players){
+        updateSquares(players);
     });
 
+
     $(window).keydown(function(event){
-        var movement = {
-            vel_x: 0,
-            vel_y: 0
-        };
+
+        var movement ={
+            socket_id: my_id,
+            vel_x:0,
+            vel_y:0,
+        }
         if(event.which == 39){ //right
             console.log("right ->")
             movement.vel_x = 20;
+            socket.emit('moving',movement)
         }
         if(event.which == 37){ //left
             console.log("<- left")
             movement.vel_x = -20;
+            socket.emit('moving',movement)
         }
         if(event.which == 38){ //up
             console.log("up ^")
             movement.vel_y = -20;
+            socket.emit('moving',movement)
         }
         if(event.which == 40){ //down
             console.log("down \\/")
             movement.vel_y = 20;
+            socket.emit('moving',movement)
         }
-        socket.emit('moving',movement)
+
     })
 
+    function updateSquares(players){
+        context.clearRect(0,0,canvas.width,canvas.height)
+        $.each(players,function(index,player){
 
+            context.beginPath();
+            context.rect(player.pos.x, player.pos.y, WIDTH, LENGTH);
+            context.stroke();
+            context.closePath();
+        })
+    }
+
+/*
     function moveSquare(movement){
 
         position_x = position_x + movement.vel_x;
@@ -65,7 +118,7 @@
         context.stroke();
         context.closePath();
 
-    }
+    }*/
 
 
 })();
